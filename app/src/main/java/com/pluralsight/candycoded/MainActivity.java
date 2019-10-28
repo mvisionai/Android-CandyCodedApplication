f3b7f3e7f9f47d44fb3b874e39e1ec02bfd43658
mvisionai <vision2bernard2015@gmail.com>
2019-10-28 20:52:08 +0800

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
                         SQLiteDatabase db = candyDbHelper.getWritableDatabase();
                         Cursor cursor = db.rawQuery("SELECT * FROM candy", null);
-                        //adapter.changeCursor(cursor);
+                        adapter.changeCursor(cursor);
                     }
                 });
     }
